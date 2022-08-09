c3aba8e51b71f259a667f8c7bf41a993a4c87051
t-fujisaka <t-fujisaka@daiichi-g.co.jp>
2022-08-09 11:18:46 +0900

    PR一覧を取得

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const token: string = core.getInput('github-token')
+    const owner = github.context.repo.owner
+    const repo = github.context.repo.repo
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const octokit = github.getOctokit(token)
+    const pulls = await octokit.rest.pulls.list({
+      owner,
+      repo,
+      state: 'open'
+    })
 
-    core.setOutput('time', new Date().toTimeString())
+    core.debug(`pulls: ${JSON.stringify(pulls, null, '  ')}`)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
